b638db548c13c6f6f720f25869df393ab2fd6234
Mugezu <mugezu2013@gmail.com>
2016-11-08 22:16:27 +0200

    Добавлен вывод всех IP-адрессов сервера

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,19 +2,36 @@
  * Created by user on 08.09.2016.
  */
 
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Vector;
 
 public class Server {
     public static void main(String[] ar) throws UnknownHostException {
         int port = 6666;
-        InetAddress addr = InetAddress.getLocalHost();
-        String myLANIP = addr.getHostAddress();
-        System.out.println("Local IP-address: "+myLANIP);
+        String ip;
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface iface = interfaces.nextElement();
+                // filters out 127.0.0.1 and inactive interfaces
+                if (iface.isLoopback() || !iface.isUp())
+                    continue;
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                while(addresses.hasMoreElements()) {
+                    InetAddress addr = addresses.nextElement();
+                    ip = addr.getHostAddress();
+                    System.out.println(iface.getDisplayName() + " " + ip);
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+       // InetAddress addr = InetAddress;
+       // String myLANIP = addr.getHostAddress();
+      //  System.out.println("Local IP-address: "+myLANIP);
         try {
             List<ServerSocket> a = new Vector<>();
             List<Runnable> r = new ArrayList<>();
