b2062ad561943baf059f8e6a5d8bb95f9a85e19d
Mugezu <mugezu2013@gmail.com>
2016-11-12 13:00:15 +0200

    Добавленная поддержка внешнего IP

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -16,22 +16,18 @@ public class Server {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
             while (interfaces.hasMoreElements()) {
                 NetworkInterface iface = interfaces.nextElement();
-                // filters out 127.0.0.1 and inactive interfaces
                 if (iface.isLoopback() || !iface.isUp())
                     continue;
                 Enumeration<InetAddress> addresses = iface.getInetAddresses();
                 while(addresses.hasMoreElements()) {
                     InetAddress addr = addresses.nextElement();
                     ip = addr.getHostAddress();
-                    System.out.println(iface.getDisplayName() + " " + ip);
+                    System.out.println(iface.getDisplayName() + " " + ip +" ");
                 }
             }
         } catch (SocketException e) {
             throw new RuntimeException(e);
         }
-       // InetAddress addr = InetAddress;
-       // String myLANIP = addr.getHostAddress();
-      //  System.out.println("Local IP-address: "+myLANIP);
         try {
             List<ServerSocket> a = new Vector<>();
             List<Runnable> r = new ArrayList<>();
